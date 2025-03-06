36ed033d0647546f7b50cb503c5cc88764498f82
Ian <iankruze60@gmail.com>
2025-03-07 02:33:17 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+  const returnFirstTwoDrivers = (arrayOfDrivers) => {
+    return arrayOfDrivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = (arrayOfDrivers) => {
+    return arrayOfDrivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = (multiplier) => {
+    return (fare) => {
+      return fare * multiplier;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = (arrayOfDrivers, callback) => {
+    return callback(arrayOfDrivers);
+  };
+  
+ 
